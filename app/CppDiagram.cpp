77b7717e44aea35189d8db5b4128f896a45a240d
WBurzynski <w-burzynski@o2.pl>
2020-08-10 22:15:18 +0200

    add app folder

---
+++ app/CppDiagram.cpp
@@ -0,0 +1,18 @@
+#include "wx/wx.h"
+#include "MainFrame.hpp"
+
+class App : public wxApp
+{
+
+public:
+	virtual bool OnInit();
+};
+
+
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit()
+{
+	MainFrame* frame = new MainFrame(nullptr, wxID_ANY, wxT("Class Designer"), wxDefaultPosition, wxSize(640, 480), wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL);
+	return true;
+}
